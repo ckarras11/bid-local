feb30b867ae49e8c5652f14b7ccb2b3e5a2988e0
Christopher Karras <ckarras11@gmail.com>
2018-06-07 21:06:34 -0400

    Added footer and navbar to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import TopNav from './top-nav'
+import Footer from './footer';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">  
-
+        <TopNav />
+        <Footer />
       </div>
     );
   }
