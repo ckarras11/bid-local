23371264af90fbb5c03627484c2c1101395d88ec
Christopher Karras <ckarras11@gmail.com>
2018-06-07 21:06:23 -0400

    Added a footer

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li>FAQ</li>
+                <li>Privacy Policy</li>
+                <li>Terms of Use</li>
+                <li>Something</li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
