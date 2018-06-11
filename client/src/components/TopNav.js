5577e7ba46c400acb6fb8a370af7ad5afc760374
Christopher Karras <ckarras11@gmail.com>
2018-06-10 21:47:13 -0400

    Rendered nav bar links based on user being authenticated or not

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { logout } from '../actions';
 import '../styles/top-nav.css';
 
-const TopNav = () => {
-    return (
-        <nav>
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.reducer.isAuthenticated
+    }
+}
+
+class TopNav extends Component {
+    logout(e)  {
+        this.props.dispatch(logout);
+    }
+
+    render() {
+        const userLinks = (
             <ul>
-                <li><a href="/">Home</a></li>
                 <li><a href="/category">Categories</a></li>
                 <li>Saved</li>
                 <li>My Profile</li>
+                <li><a href="" onClick={this.logout.bind(this)} >Logout</a></li>
             </ul>
-        </nav>
-    );
-};
+        );
 
-export default TopNav;
+        const guestLinks = (
+            <ul>
+                <li><a href="/signup">Signup</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        );
+
+        return (
+            <nav>
+                {this.props.isAuthenticated ? userLinks : guestLinks}
+            </nav>
+        )
+    }
+}
+
+export default connect(mapStateToProps)(TopNav);
