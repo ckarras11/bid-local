0f23c18d2e90856823717fa9ec724e9482592de4
Christopher Karras <ckarras11@gmail.com>
2018-06-10 21:50:53 -0400

    Added functions to set current user and auth headers if token exists

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import {setCurrentUser} from './actions';
+import jwt from 'jsonwebtoken';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import setAuthorizationToken from './utils/setAuthToken';
+
+if (sessionStorage.jwtToken) {
+	setAuthorizationToken(sessionStorage.jwtToken);
+	store.dispatch(setCurrentUser(jwt.decode(sessionStorage.jwtToken)));
+}
 
 ReactDOM.render(
-        <App />
-        , document.getElementById('root'));
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
