8c99eed73e4cf97b9462908041d223f138c75655
Christopher Karras <ckarras11@gmail.com>
2018-06-11 20:21:09 -0400

    Added checkToken middleware function to authenticate user

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const { DATABASE_URL, PORT, JWT_ENCRYPTION_KEY } = require('./config');
 const path = require('path');
 const { User } = require('./models/user');
 
-
-
 // Initializing app
 const app = express();
 mongoose.Promise = global.Promise;
@@ -23,16 +21,22 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 function checkToken(req, res, next) {
-    const token = req.headers['x-access-token'];
-    if(!token) {
-        console.log('No token provided');
-        return res.status(401).send({auth: false, message: 'Invalid Credentials'})
+    const authorizationHeader = req.headers['authorization'];
+    let token;
+
+    if (authorizationHeader) {
+        token = authorizationHeader.split(' ')[1];
     }
 
-    jwt.verify(token, process.env.JWT_ENCRYPTION_KEY, function(err, decoded) {
+    if(!token) {
+        console.log('No token provided');
+        return res.status(403).send({auth: false, message: 'Missing Token'})
+    }
+
+    jwt.verify(token, JWT_ENCRYPTION_KEY, function(err, decoded) {
         if (err){
             console.log('Failed to authenticate');
-            return res.status(500).res.send({ auth: false, message: 'Failed to authenticate.' });
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
         }
         else{
             console.log('Decoded token is: '+decoded);
@@ -66,7 +70,7 @@ app.post('/api/auth', (req, res) => {
 })
 
 app.get('/api/hello', checkToken, (req, res) => {
-    res.send({ express: 'Hello From Express' });
+    res.status(200).send({ express: 'Hello From Express' });
   });
 // Initializing Server
 let server;
