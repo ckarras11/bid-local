42b88f7de8748e8775f7515c801fedb6edcb31b0
Christopher Karras <ckarras11@gmail.com>
2018-06-16 21:22:42 -0400

    Moved logic and dispatch from LoginForm to LoginPage

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import LoginForm from './loginForm';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { login } from '../../actions';
 
-export default class LoginPage extends Component {
-    render() {
-        return (
-            <div>
-                <LoginForm/>
-            </div>
-        );
-    }
+export class LoginPage extends Component {
+	constructor(props) {
+		super(props);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onSubmit(values) {
+		this.props.dispatch(login(values));
+	}
+
+	render() {
+		if (this.props.isAuthenticated) {
+			return <Redirect to="/Browse" />;
+		}
+
+		return (
+			<div>
+				<LoginForm onSubmit={this.onSubmit} err={this.props.errorMsg} />
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = state => {
+	return {
+		errorMsg: state.reducer.errorMsg,
+		isAuthenticated: state.reducer.isAuthenticated
+	};
+};
+
+export default connect(mapStateToProps)(LoginPage);
