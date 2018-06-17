bb1e79df01ff9f93bf6e31f23974e999251e3366
Christopher Karras <ckarras11@gmail.com>
2018-06-16 21:58:44 -0400

    Added fields to user schema

--- models/user.js
+++ models/user.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	terms: { type: Boolean, required: true }
 });
-
 
 const User = mongoose.model('User', userSchema, 'User');
 
