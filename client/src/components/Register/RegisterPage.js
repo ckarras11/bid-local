63a42b0a9ec8cee2766633812b298c87f3df376d
Christopher Karras <ckarras11@gmail.com>
2018-06-16 21:23:42 -0400

    Moved mapStateToProps from RegisterForm to RegisterPage

--- client/src/components/Register/RegisterPage.js
+++ client/src/components/Register/RegisterPage.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import RegisterForm from './registerForm';
+import { connect } from 'react-redux';
 
-export default class RegisterPage extends Component {
-constructor(props) {
-  super(props);
-  this.onSubmit = this.onSubmit.bind(this);
-}
+export class RegisterPage extends Component {
+	constructor(props) {
+		super(props);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
 
-
-  onSubmit(values) {
-    /* this.props.dispatch(login({
+	onSubmit(values) {
+		/* this.props.dispatch(login({
         username: this.state.username,
         password: this.state.password
     })) */
-    console.log(values)
+		console.log(values);
+	}
+	render() {
+		return (
+			<div>
+				<RegisterForm onSubmit={this.onSubmit} />
+			</div>
+		);
+	}
 }
-  render() {
-    return (
-      <div>
-        <RegisterForm onSubmit={this.onSubmit}/>
-      </div>
-    )
-  }
-}
+
+const mapStateToProps = state => {
+	return {
+		errorMsg: state.reducer.errorMsg,
+		isAuthenticated: state.reducer.isAuthenticated
+	};
+};
+
+export default connect(mapStateToProps)(RegisterPage);
