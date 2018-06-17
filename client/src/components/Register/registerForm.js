43ba86e05d1a10e5c91917f5952fca3fcd9b38b0
Christopher Karras <ckarras11@gmail.com>
2018-06-16 21:20:52 -0400

    made presentational component and removed mapStateToProps

--- client/src/components/Register/registerForm.js
+++ client/src/components/Register/registerForm.js
@@ -1,69 +1,85 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import renderField from '../renderField';
 import {
-    required,
-    maxLength15,
-    minLength2,
-    alphaNumeric,
-    aol,
-    number,
-    minValue18,
-    tooOld,
-    phoneNumber,
-    email
+	required,
+	maxLength15,
+	minLength2,
+	alphaNumeric,
+	email
 } from '../../utils/formValidation';
 
-let RegisterForm = (props) => {
-    const { handleSubmit} = props
-    return (
-        <form className={'form-horizontal'} onSubmit={handleSubmit}>
-            <Field
-                name="username"
-                type="text"
-                component={renderField}
-                label="Username"
-                validate={[required, maxLength15, minLength2]}
-                warn={alphaNumeric}
-            />
-            <Field
-                name="email"
-                type="email"
-                component={renderField}
-                label="Email"
-                validate={email}
-                warn={aol}
-            />
-            <Field
-                name="age"
-                type="number"
-                component={renderField}
-                label="Age"
-                validate={[required, number, minValue18]}
-                warn={tooOld}
-            />
-            <Field
-                name="phone"
-                type="number"
-                component={renderField}
-                label="Phone number"
-                validate={[required, phoneNumber]}
-            />
-            <button className={'btn btn-success'} type='submit'>Submit</button>
-        </form>
-    )
-  }
+let RegisterForm = props => {
+	const { handleSubmit, err } = props;
+	return (
+		<form className={'form-horizontal'} onSubmit={handleSubmit}>
+			<p>{err}</p>
+			<h2>Sign Up</h2>
+			<p>Create an account. It's free and only takes a second!</p>
+			<div className="fields">
+				<div className="name">
+					<Field
+						name="first"
+						type="text"
+						component={renderField}
+						label="First Name"
+						validate={[required, maxLength15, minLength2]}
+						warn={alphaNumeric}
+					/>
+					<Field
+						name="last"
+						type="text"
+						component={renderField}
+						label="Last Name"
+						validate={[required, maxLength15, minLength2]}
+						warn={alphaNumeric}
+					/>
+				</div>
+				<Field
+					name="email"
+					type="email"
+					component={renderField}
+					label="Email"
+					validate={[required, email]}
+				/>
+				<Field
+					name="password"
+					type="password"
+					component={renderField}
+					label="Password"
+					validate={[required]}
+				/>
+				<Field
+					name="password2"
+					type="password2"
+					component={renderField}
+					label="Confirm Password"
+					validate={[required]}
+				/>
+				<div className="terms-container">
+					<Field
+						name="terms"
+						type="checkbox"
+						component={renderField}
+						label="I accept the Terms"
+						validate={[required]}
+					/>
+					<p className="terms">
+						I accept the <a href="">Terms of Use</a> &{' '}
+						<a href="">Privacy Policy</a>
+					</p>
+				</div>
 
-const mapStateToProps = state =>  {
-    return {
-        errorMsg: state.reducer.errorMsg,
-        isAuthenticated: state.reducer.isAuthenticated
-    };
-}
+				<button className={'btn'} type="submit">
+					Register Now
+				</button>
+			</div>
+		</form>
+	);
+};
 
 RegisterForm = reduxForm({
-    form: 'register'
-})(RegisterForm)
+	form: 'register'
+})(RegisterForm);
 
-export default connect(mapStateToProps)(RegisterForm)
+export default RegisterForm;
