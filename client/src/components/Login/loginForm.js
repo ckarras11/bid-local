a25edba8a159ecdb1abb664662e7d2f21b4585ce
Christopher Karras <ckarras11@gmail.com>
2018-06-16 21:21:46 -0400

    Used redux form and made functional component. Moved logic to LoginPage

--- client/src/components/Login/loginForm.js
+++ client/src/components/Login/loginForm.js
@@ -1,63 +1,39 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import '../../styles/loginForm.css';
-import { login } from '../../actions';
+import React from 'react';
+import '../../styles/form.css';
+import { Field, reduxForm } from 'redux-form';
+import renderField from '../renderField';
+import { required, email } from '../../utils/formValidation';
 
+let LoginForm = props => {
+	const { handleSubmit, err } = props;
 
-export class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            errors: {},
-            isLoading: false
-        }
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
-    
-    onSubmit(e) {
-        e.preventDefault();
-        this.props.dispatch(login({
-            username: this.state.username,
-            password: this.state.password
-        }))
-    }
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-    
-    render() {
-        const {username, password, isLoading} = this.state
-        if (this.props.isAuthenticated) {
-            return <Redirect to='/Browse' />
-        }
-        return (
-            <form className={'form-horizontal'} onSubmit={this.onSubmit}>
-                <p>{this.props.errorMsg}</p>
-                <legend>Log In</legend>
-                <div className={'form-group'}>
-                    <label className={'control-label'} htmlFor='username'>Username:</label>
-                    <input className='form-control' onChange={this.onChange} value={username} name='username' type='text' />
-                </div>
-                <div className={'form-group'}>
-                    <label className={'control-label'} htmlFor='password'>Password:</label>
-                    <input className='form-control' onChange={this.onChange} value={password} name='password' type='password' />
-                </div>
-                <button className={'btn btn-success'} type='submit' disabled={isLoading} >Submit</button>
-
-            </form>
-        );
-    }
+	return (
+		<form className={'form-horizontal'} onSubmit={handleSubmit}>
+			<p>{err}</p>
+			<h2>Log In</h2>
+			<Field
+				name="email"
+				type="email"
+				component={renderField}
+				label="Email"
+				validate={[required, email]}
+			/>
+			<Field
+				name="password"
+				type="password"
+				component={renderField}
+				label="Password"
+				validate={[required]}
+			/>
+			<button className={'btn'} type="submit">
+				Submit
+			</button>
+		</form>
+	);
 };
 
-const mapStateToProps = state =>  {
-    return {
-        errorMsg: state.reducer.errorMsg,
-        isAuthenticated: state.reducer.isAuthenticated
-    };
-}
+LoginForm = reduxForm({
+	form: 'login'
+})(LoginForm);
 
-export default connect(mapStateToProps)(LoginForm)
+export default LoginForm;
