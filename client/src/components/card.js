d111024c61b9e7bd5e4ec7ef8e6d8c0955420fb5
Christopher Karras <ckarras11@gmail.com>
2018-07-07 22:16:11 -0400

    added price to card and changed how the image was displayed

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../styles/card.css';
 
-const Card = (props) => {
-    return (
-        <div className="card">
-            <div className="image"></div>
-            <div className="description">
-                <p className="item-title">{props.title}</p>
-                <p className="date-loc">3 days ago - Harwich, MA</p>
-            </div>
-        </div>
-    );
+const Card = props => {
+	return (
+		<div className="card">
+			<img
+				src="https://scontent.fbed1-2.fna.fbcdn.net/v/t1.0-0/s600x600/34688646_10214679588814701_3669100727436836864_n.jpg?_nc_cat=0&oh=a3deb301485cf57e0b078cafeb5314c1&oe=5BAADBB4"
+				alt=""
+			/>
+			<p className="price">${props.price}</p>
+			<div className="description">
+				<p className="item-title">{props.title}</p>
+				<p className="date-loc">3 days ago - Harwich, MA</p>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
--- client/src/components/grid.js
+++ client/src/components/grid.js
@@ -1,81 +1,86 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Card from './card';
 import '../styles/grid.css';
+import axios from 'axios';
 
 const tilesData = [
-    {
-      img: 'images/grid-list/00-52-29-429_640.jpg',
-      title: 'Breakfast',
-      author: 'jill111',
-    },
-    {
-      img: 'images/grid-list/burger-827309_640.jpg',
-      title: 'Tasty burger',
-      author: 'pashminu',
-    },
-    {
-      img: 'images/grid-list/camera-813814_640.jpg',
-      title: 'Camera',
-      author: 'Danson67',
-    },
-    {
-      img: 'images/grid-list/morning-819362_640.jpg',
-      title: 'Morning',
-      author: 'fancycrave1',
-    },
-    {
-      img: 'images/grid-list/hats-829509_640.jpg',
-      title: 'Hats',
-      author: 'Hans',
-    },
-    {
-      img: 'images/grid-list/honey-823614_640.jpg',
-      title: 'Honey',
-      author: 'fancycravel',
-    },
-    {
-      img: 'images/grid-list/vegetables-790022_640.jpg',
-      title: 'Vegetables',
-      author: 'jill111',
-    },
-    {
-      img: 'images/grid-list/water-plant-821293_640.jpg',
-      title: 'Water plant',
-      author: 'BkrmadtyaKarki',
-    },
+	{
+		img: 'images/grid-list/00-52-29-429_640.jpg',
+		title: 'Breakfast',
+		author: 'jill111',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/burger-827309_640.jpg',
+		title: 'Tasty burger',
+		author: 'pashminu',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/camera-813814_640.jpg',
+		title: 'Camera',
+		author: 'Danson67',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/morning-819362_640.jpg',
+		title: 'Morning',
+		author: 'fancycrave1',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/hats-829509_640.jpg',
+		title: 'Hats',
+		author: 'Hans',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/honey-823614_640.jpg',
+		title: 'Honey',
+		author: 'fancycravel',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/vegetables-790022_640.jpg',
+		title: 'Vegetables',
+		author: 'jill1111',
+		price: 25.99
+	},
+	{
+		img: 'images/grid-list/water-plant-821293_640.jpg',
+		title: 'Water plant',
+		author: 'BkrmadtyaKarki',
+		price: 25.99
+	}
 ];
 
 export default class Grid extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			response: ''
+		};
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      response: ''
-    }
-  }
+	componentDidMount() {
+		this.callApi()
+			.then(res => this.setState({ response: res.express }))
+			.catch(err => console.log(err));
+	}
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
+	callApi = async () => {
+		const response = await axios.get('/api/hello');
+		const body = await response.json();
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+		if (response.status !== 200) throw Error(body.message);
 
-    if (response.status !== 200) throw Error(body.message);
+		return body;
+	};
 
-    return body;
-  };
-
-  render() {
-    console.log(this.state)
-      let cards = tilesData.map(item => <Card title={item.title}/>)
-    return (
-      <div className="grid-container">
-        {cards}
-      </div>
-    )
-  }
+	render() {
+		let cards = tilesData.map(item => (
+			<Card key={item.author} title={item.title} price={item.price} />
+		));
+		return <div className="grid-container">{cards}</div>;
+	}
 }
