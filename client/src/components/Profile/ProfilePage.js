410e46e3ba63efffdc051cc379bb6c76da0e92e8
Christopher Karras <ckarras11@gmail.com>
2018-07-20 21:42:11 -0400

    Added modal to profile page to add items for sale

--- client/src/components/Profile/ProfilePage.js
+++ client/src/components/Profile/ProfilePage.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import Grid from '../grid';
+import Modal from '../modal';
+import { connect } from 'react-redux';
+import { toggleModal } from '../../actions';
 import '../../styles/profile.css';
 
 class ProfilePage extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.props.dispatch(toggleModal());
+	}
+
 	componentWillMount() {
 		console.log('getting posted items');
 	}
 	render() {
+		let modal;
+		if (this.props.showModal) {
+			modal = <Modal />;
+		}
 		return (
 			<div className="profile">
+				{modal}
 				<div className="profile_top">
 					<h1>Hello Christopher</h1>
-					<button className="btn">Sell Something</button>
+					<button onClick={this.handleClick} className="btn">
+						Sell Something
+					</button>
 				</div>
 				<h3>Here are your current Items:</h3>
 				<Grid />
@@ -20,4 +39,10 @@ class ProfilePage extends Component {
 	}
 }
 
-export default ProfilePage;
+const mapStateToProps = state => {
+	return {
+		showModal: state.reducer.showModal
+	};
+};
+
+export default connect(mapStateToProps)(ProfilePage);
