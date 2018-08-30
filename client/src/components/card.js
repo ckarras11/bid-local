1b024d083a2ff9a7f2f0972c63ee5ad4c84fed54
Christopher Karras <ckarras11@gmail.com>
2018-08-29 22:18:07 -0400

    Added PropTypes and pulled real data from db

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import '../styles/card.css';
+import PropTypes from 'prop-types';
 
 const Card = props => {
 	return (
 		<div className="card">
-			<img
-				src="https://scontent.fbed1-2.fna.fbcdn.net/v/t1.0-0/s600x600/34688646_10214679588814701_3669100727436836864_n.jpg?_nc_cat=0&oh=a3deb301485cf57e0b078cafeb5314c1&oe=5BAADBB4"
-				alt=""
-			/>
+			<img src={props.image} alt="" />
 			<p className="price">${props.price}</p>
 			<div className="description">
 				<p className="item-title">{props.title}</p>
-				<p className="date-loc">3 days ago - Harwich, MA</p>
+				<p className="date-loc">3 days ago - {props.location}</p>
 			</div>
 		</div>
 	);
+};
+
+Card.propTypes = {
+	title: PropTypes.string.isRequired,
+	location: PropTypes.string,
+	price: PropTypes.number.isRequired
+};
+
+Card.defaultProps = {
+	location: 'N/A'
 };
 
 export default Card;
--- client/src/components/grid.js
+++ client/src/components/grid.js
@@ -3,83 +3,40 @@ import Card from './card';
 import '../styles/grid.css';
 import axios from 'axios';
 
-const tilesData = [
-	{
-		img: 'images/grid-list/00-52-29-429_640.jpg',
-		title: 'Breakfast',
-		author: 'jill111',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/burger-827309_640.jpg',
-		title: 'Tasty burger',
-		author: 'pashminu',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/camera-813814_640.jpg',
-		title: 'Camera',
-		author: 'Danson67',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/morning-819362_640.jpg',
-		title: 'Morning',
-		author: 'fancycrave1',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/hats-829509_640.jpg',
-		title: 'Hats',
-		author: 'Hans',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/honey-823614_640.jpg',
-		title: 'Honey',
-		author: 'fancycravel',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/vegetables-790022_640.jpg',
-		title: 'Vegetables',
-		author: 'jill1111',
-		price: 25.99
-	},
-	{
-		img: 'images/grid-list/water-plant-821293_640.jpg',
-		title: 'Water plant',
-		author: 'BkrmadtyaKarki',
-		price: 25.99
-	}
-];
-
 export default class Grid extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			response: ''
+			response: '',
+			items: []
 		};
 	}
 
 	componentDidMount() {
 		this.callApi()
-			.then(res => this.setState({ response: res.express }))
+			.then(res =>
+				this.setState({ response: res.status, items: [...res.data] })
+			)
 			.catch(err => console.log(err));
 	}
 
 	callApi = async () => {
-		const response = await axios.get('/api/hello');
-		const body = await response.json();
-
+		const response = await axios.get('/api/for_sale');
+		const body = await response;
 		if (response.status !== 200) throw Error(body.message);
-
 		return body;
 	};
 
 	render() {
-		let cards = tilesData.map(item => (
-			<Card key={item.author} title={item.title} price={item.price} />
+		console.log(this.state);
+		let cards = this.state.items.map(item => (
+			<Card
+				key={item._id}
+				location={item.location}
+				title={item.title}
+				price={item.price}
+				image={item.upload.secure_url}
+			/>
 		));
 		return <div className="grid-container">{cards}</div>;
 	}
