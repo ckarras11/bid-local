0740153926f8cd06d1895dbbaa2201fc1b053fb2
Christopher Karras <ckarras11@gmail.com>
2018-09-30 12:11:29 -0400

    Added terms of use

---
+++ client/src/components/Terms.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import '../styles/privacy.css';
+
+const Terms = () => {
+	return (
+		<div className="terms__container">
+			<h2>Terms of Use</h2>
+			<p>
+				By accessing or using the Bid Cape Cod's website, service, or any
+				applications (including mobile applications) made available by Bid Cape
+				Cod you agree to be bound by these terms of use (“Terms of Use”). The
+				Service is owned or controlled by Bid Cape Cod. These Terms of Use
+				affect your legal rights and obligations. If you do not agree to be
+				bound by all of these Terms of Use, do not access or use the Service.
+			</p>
+			<h3>Summary</h3>
+			<ul>
+				<li>You must be at least 18 years old to open an account.</li>
+				<li>
+					Meeting people online can be dangerous and all percautions should be
+					taken by the user
+				</li>
+				<li>
+					Bid Cape Cod was created to be used on Cape Cod and the Islands as
+					well as the South Shore
+				</li>
+				<li>
+					You are solely responsible for protecting your own account password
+					and other account information.
+				</li>
+				<li>
+					Bid Cape Cod has no part of the actual sale or physical goods.
+					Therefore no warranty, refunds, issues, or anything else pertaining to
+					a sale provided by Bid Cape Cod.
+				</li>
+				<li>
+					Do not use Bid Cape Cod if you do not agree to the Terms of Use
+					described below. Your use of Bid Cape Cod means you agree to these
+					Terms of Use.
+				</li>
+			</ul>
+			<h3>Physical Goods</h3>
+			<p>
+				Bid Cape Cod has no part in the actual sale/transaction of goods. At no
+				point will Bid Cape Cod be liable for anything. All precautions are up
+				to the buyer and seller. There is no warranty or refunds implied.
+			</p>
+			<h3>Termination Disclosure</h3>
+			<p>
+				We may terminate your access to the Site, without cause or notice, which
+				may result in the forfeiture and destruction of all information
+				associated with you. All provisions of this Agreement that by their
+				nature should survive termination shall survive termination, including,
+				without limitation, ownership provisions, warranty disclaimers,
+				indemnity, and limitations of liability.
+			</p>
+			<h3>Intellectual Property Disclosure</h3>
+			<p>
+				This website and its original content, features, and functionality are
+				owned by Bid Cape Cod and are protected by international copyright,
+				trademark, patent, trade secret and other intellectual property or
+				proprietary rights laws.
+			</p>
+		</div>
+	);
+};
+
+export default Terms;
