5b8d081fc06b8cde54118e8ef88feb4fd95266a3
Christopher Karras <ckarras11@gmail.com>
2018-09-30 11:39:34 -0400

    Added a FAQ section

---
+++ client/src/components/Faq.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../styles/privacy.css';
+const Faq = () => {
+	return (
+		<div className="faq__container">
+			<h2>Frequently Asked Questions</h2>
+			<hr />
+			<h3>What is Bid Cape Cod?</h3>
+			<p>
+				Bid Cape Cod is a solution for local autions and sales. Instead of
+				listing an item to a local group on a first come first serve basis, list
+				it on Bid Cape Cod to make the most from selling your stuff. This gives
+				all buyers an equal opportunity to purchase something. If they want it
+				bad enough they can pay for it. This way they get the item they want and
+				you get the most for it!
+			</p>
+			<h3>How do I use it?</h3>
+			<p>
+				To list an item or browse items for sale simply create an account (it's
+				free!). From here you will be redirected to the browse page to see all
+				items currently for sale. To post an item simply go to the profile page,
+				click "Sell Something", upload a picture, fill out the form and submit.
+				Make sure to include contact information so buyers can contact you.
+			</p>
+			<h3>Do I need to pay for it?</h3>
+			<p>
+				At the moment Bid Cape Cod is completley free. Since the service is free
+				you are limited on the number of pictures you can upload. In the future
+				this may change depending on use.
+			</p>
+			<h3>What is the future of this app?</h3>
+			<p>
+				This was made as a project for fun. If people happen to start using it
+				then I will begin maintaining and implementing new features.
+			</p>
+		</div>
+	);
+};
+
+export default Faq;
