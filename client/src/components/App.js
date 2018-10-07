9ca1f256eafd9f40b402f6ad080dcab7b4526080
Christopher Karras <ckarras11@gmail.com>
2018-10-07 14:12:08 -0400

    Added FAQ, Terms and Privacy statement routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,9 @@ import Grid from './grid';
 import LoginPage from './Login/LoginPage';
 import RegisterPage from './Register/RegisterPage';
 import ProfilePage from './Profile/ProfilePage';
+import Faq from './Faq';
+import Terms from './Terms';
+import Privacy from './Privacy';
 import setAuthorizationToken from '../utils/setAuthToken';
 import requireAuth from '../utils/requireAuth';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -22,6 +25,9 @@ class App extends Component {
 					<Route exact path="/" component={HomePage} />
 					<Route path="/login" component={LoginPage} />
 					<Route path="/register" component={RegisterPage} />
+					<Route path="/faq" component={Faq} />
+					<Route path="/terms" component={Terms} />
+					<Route path="/privacy" component={Privacy} />
 					<Route path="/profile" component={requireAuth(ProfilePage)} />
 					<Route path="/browse" component={requireAuth(Grid)} />
 					<Footer />
