0513a34010bd6ed7a83aad3c8c6601bd35daa0d3
Christopher Karras <ckarras11@gmail.com>
2018-10-07 14:12:44 -0400

    Added links for privacy policy, faq, and terms to footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import '../styles/footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
-        <footer>
-            <ul>
-                <li>FAQ</li>
-                <li>Privacy Policy</li>
-                <li>Terms of Use</li>
-                <li>Something</li>
-            </ul>
-        </footer>
-    );
+	return (
+		<footer>
+			<ul>
+				<li>
+					<Link to="/faq">FAQ</Link>
+				</li>
+				<li>
+					<Link to="/privacy">Privacy Policy</Link>
+				</li>
+				<li>
+					<Link to="/terms">Terms of Use</Link>
+				</li>
+			</ul>
+		</footer>
+	);
 };
 
 export default Footer;
